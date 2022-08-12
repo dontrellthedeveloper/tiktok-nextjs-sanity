924f3de05adc9174b744d306e82edc97851fac96
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-08-12 11:46:53 -0400

    added api for liking/unliking post

---
+++ components/LikeButton.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { MdFavorite } from 'react-icons/md';
+import { NextPage } from 'next';
+
+import useAuthStore from '../store/authStore';
+
+interface IProps {
+    likes: any;
+    flex: string;
+    handleLike: () => void;
+    handleDislike: () => void;
+}
+
+const LikeButton = ({ handleLike, handleDisLike}: IProps) => {
+    const [alreadyLiked, setAlreadyLiked] = useState(false);
+    const { userProfile }: any = useAuthStore();
+
+    return (
+        <div className={`gap-6`}>
+            <div className='mt-4 flex flex-col justify-center items-center cursor-pointer'>
+                {alreadyLiked ? (
+                    <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] ' onClick={handleDislike} >
+                        <MdFavorite className='text-lg md:text-2xl' />
+                    </div>
+                ) : (
+                    <div className='bg-primary rounded-full p-2 md:p-4 ' onClick={handleLike} >
+                        <MdFavorite className='text-lg md:text-2xl' />
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default LikeButton;
