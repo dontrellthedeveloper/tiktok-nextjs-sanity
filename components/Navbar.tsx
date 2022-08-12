a1068cf26ce66724d62fb6c5d5df41c5b3887bde
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-08-12 08:33:01 -0400

    added user store with google oauth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,10 +7,14 @@ import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 
+import useAuthStore from '../store/authStore';
 import Logo from '../utils/tiktok-logo.png';
+import {createOrGetUser} from "../utils";
 
 const Navbar = () => {
-    const user = false;
+    const { userProfile, addUser, removeUser } = useAuthStore();
+
+    // @ts-ignore
     return (
         <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
             <Link href='/'>
@@ -25,11 +29,41 @@ const Navbar = () => {
             </Link>
             <div>Search</div>
             <div>
-                {user ? (
-                    <div>Loggedd In</div>
+                {userProfile ? (
+                    <div className='flex gap-5 md:gap-10'>
+                        <Link href='/upload'>
+                            <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+                                <IoMdAdd className='text-xl' />{' '}
+                                <span className='hidden md:block'>Upload </span>
+                            </button>
+                        </Link>
+                        {userProfile.image && (
+                            <Link href={`/`}>
+                                <>
+                                    <Image
+                                        className='rounded-full cursor-pointer'
+                                        src={userProfile.image}
+                                        alt='user'
+                                        width={40}
+                                        height={40}
+                                    />
+                                </>
+                            </Link>
+                        )}
+                        <button
+                            type='button'
+                            className=' border-2 p-2 rounded-full cursor-pointer outline-none shadow-md'
+                            onClick={() => {
+                                googleLogout();
+                                removeUser();
+                            }}
+                        >
+                            <AiOutlineLogout color='red' fontSize={21} />
+                        </button>
+                    </div>
                 ) : (
                     <GoogleLogin
-                        onSuccess={(response) => console.log(response) }
+                        onSuccess={(response) => createOrGetUser(response, addUser) }
                         onError={() => console.log('Login Failed')}
                     />
                 )}
--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['i.ibb.co']
+    domains: ['i.ibb.co', 'lh3.googleusercontent.com']
   }
 }
