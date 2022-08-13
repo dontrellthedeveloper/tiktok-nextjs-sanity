8da68c0643e987901ca4085c0395bf58b9554033
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-08-13 14:27:15 -0400

    added the ability to search for videos using searchTerm api

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,6 +13,17 @@ import {createOrGetUser} from "../utils";
 
 const Navbar = () => {
     const { userProfile, addUser, removeUser }: any = useAuthStore();
+    const [searchValue, setSearchValue] = useState('');
+    const router = useRouter();
+
+
+    const handleSearch = (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+
+        if(searchValue) {
+            router.push(`/search/${searchValue}`);
+        }
+    };
 
     // @ts-ignore
     return (
@@ -27,7 +38,28 @@ const Navbar = () => {
                     />
                 </div>
             </Link>
-            <div>Search</div>
+
+            <div className='relative hidden md:block'>
+                <form
+                    onSubmit={handleSearch}
+                    className='absolute md:static top-10 -left-20 bg-white'
+                >
+                    <input
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                        className='bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0'
+                        placeholder='Search accounts and videos'
+                    />
+                    <button
+                        onClick={handleSearch}
+                        className='absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400'
+                    >
+                        <BiSearch />
+                    </button>
+
+                </form>
+            </div>
+
             <div>
                 {userProfile ? (
                     <div className='flex gap-5 md:gap-10'>
---
+++ pages/search/[searchTerm].tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { GoVerified } from 'react-icons/go';
+import Link from 'next/link';
+import axios from 'axios';
+
+import NoResults from '../../components/NoResults';
+import VideoCard from '../../components/VideoCard';
+import useAuthStore from '../../store/authStore';
+import { BASE_URL } from '../../utils';
+import { IUser, Video } from '../../types';
+
+const Search = ({ videos }: { videos: Video[] }) => {
+    const [isAccounts, setIsAccounts] = useState(false);
+    const { allUsers }: { allUsers: IUser[] } = useAuthStore();
+
+    const router = useRouter();
+    const { searchTerm }: any = router.query;
+
+    const accounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
+    const isVideos = !isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
+
+    const searchedAccounts = allUsers?.filter((user: IUser) => user.userName.toLowerCase().includes(searchTerm));
+
+    console.log(videos);
+
+    return (
+        <div className='w-full  '>
+            <div className='flex gap-10 mb-10 border-b-2 border-gray-200 md:fixed z-50 bg-white w-full'>
+                <p onClick={() => setIsAccounts(true)} className={`text-xl  font-semibold cursor-pointer ${accounts} mt-2`}>
+                    Accounts
+                </p>
+                <p className={`text-xl font-semibold cursor-pointer ${isVideos} mt-2`} onClick={() => setIsAccounts(false)}>
+                    Videos
+                </p>
+            </div>
+            {isAccounts ? (
+                <div className='md:mt-16'>
+
+                </div>
+            ) : (
+                <div className='md:mt-16 flex flex-wrap gap-6 md:justify-start '>
+                    {videos.length ? (
+                        videos.map((video: Video, idx: number) => (
+                            <VideoCard post={video} key={idx} />
+                        ))
+                    ) : (
+                        <NoResults text={`No Video Results for ${searchTerm}`} />
+                    )}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export const getServerSideProps = async ({
+params: { searchTerm },
+                                         }: {
+    params: { searchTerm: string };
+}) => {
+    const res = await axios.get(`${BASE_URL}/api/search/${searchTerm}`);
+
+    return {
+        props: { videos: res.data },
+    };
+};
+
+export default Search;
