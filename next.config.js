04e548510f3039f7dedacd8561d9b09513700e07
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-08-13 15:11:16 -0400

    made changes to next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  typescript:{
+    ignoreBuildErrors: true
+  },
   reactStrictMode: true,
   images: {
     domains: ['i.ibb.co', 'lh3.googleusercontent.com']
