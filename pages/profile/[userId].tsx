7e46a7f3be29757cb5e7d9ddc92960b1974443ef
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-08-13 05:15:24 -0400

    added profile page api and starting to add profile component

---
+++ pages/profile/[userId].tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { GoVerified } from 'react-icons/go';
+import axios from 'axios';
+
+import VideoCard from '../../components/VideoCard';
+import NoResults from '../../components/NoResults';
+import { IUser, Video } from '../../types';
+import { BASE_URL } from '../../utils';
+
+
+interface IProps {
+    data: {
+        user: IUser;
+        userVideos: Video[];
+        userLikedVideos: Video[];
+    };
+}
+
+
+const Profile = ({ data }: IProps) => {
+    return (
+        <div>
+            Profile
+        </div>
+    );
+};
+
+export const getServerSideProps = async ({
+    params: { userId },
+}: {
+    params: { userId: string };
+}) => {
+    const res = await axios.get(`${BASE_URL}/api/profile/${userId}`);
+
+    return {
+        props: { data: res.data },
+    };
+};
+export default Profile;
