52563431abb06eef049786bac296607cf4c4df46
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-08-14 12:12:42 -0400

    made changes in sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,7 +12,7 @@ import Footer from './Footer';
 import {topics} from "../utils/constants";
 
 
-const Sidebar = () => {
+const Sidebar = ({href}: any) => {
     const [showSidebar, setShowSidebar] = useState<Boolean>(true)
     const router = useRouter();
     const { topic } = router.query;
@@ -38,7 +38,8 @@ const Sidebar = () => {
                 <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
                     <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
                         <Link href='/'>
-                            <div className={normalLink}>
+                            {/*<div className={normalLink}>*/}
+                            <div className={router.pathname === '/' ? activeTopicLink : topicLink}>
                                 <p className='text-3xl '>
                                     <AiOutlineHome />
                                 </p>
